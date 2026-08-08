230875a359857f116c3d8be0dc7871498747d01e
Joshua D. Graber <code+gh@joshuadgraber.com>
2026-08-08 13:15:57 -0400

    fix: next and vercel configs for queue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/queues/transcribe-microdose": ["./vercel-transcription/**/*"],
+  },
 };
 
 export default nextConfig;
